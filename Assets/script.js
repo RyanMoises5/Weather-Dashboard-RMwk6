04569e1df28e98be2f397a535ed4da63c9afe576
Ryan M <ryan.a.moises@gmail.com>
2024-02-05 00:57:29 -0500

    Working forecast cards, but getting Uncaught error

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
 var locationEl = $('#location');
 var submitBtn = $("#search-button");
+var todayStats = $("#today");
+var forecast = $("#5day-forecast");
 
 var formSubmission = function (event) {
     event.preventDefault();
@@ -7,14 +9,14 @@ var formSubmission = function (event) {
     var locationInput = locationEl.val().trim();
 
     if (locationInput) {
-        // getData(locationInput);
+        getData(locationInput);
     } else {
         alert('Please enter a location');
     }
 };
 
 var getData = function (locationInput) {
-    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + locationInput + '&appid=b70a8edb3a23f7455b534efc0f71881d';
+    var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + locationInput + '&appid=b70a8edb3a23f7455b534efc0f71881d&units=imperial';
 
     fetch(apiURL)
         .then(function (response) {
@@ -22,12 +24,61 @@ var getData = function (locationInput) {
                 console.log(response);
                 response.json().then( function (data) {
                     console.log(data);
-                    // function here //
+                    
+                    displayTodayData(data, locationInput);
+                    displayForecast(data);
                 });
             } else {
-                alert ('Error: ' + response.statusText);
+                alert ('Error: City ' + response.statusText);
             }
-        });
+
+        })
 };
+
+var displayTodayData = function (data, locationInput) {
+
+    var headerToday = $('<h1>');
+    headerToday.text(locationInput + " (" + dayjs.unix(data.list[0].dt).format("M/D/YYYY") + ")");
+    todayStats.append(headerToday);
+    
+    var tempToday = $('<p>');
+    tempToday.text("Temp: " + data.list[0].main.temp + "°F");
+    todayStats.append(tempToday);
+
+    var windToday = $('<p>');
+    windToday.text("Wind: " + data.list[0].wind.speed + " MPH");
+    todayStats.append(windToday);
+
+    var humidToday = $('<p>');
+    humidToday.text("Humidity: " + data.list[0].main.humidity + "%");
+    todayStats.append(humidToday);
+}
+
+var displayForecast = function (data) {
+
+    for (let index = 8; index <= 40; index = index + 8) {
+        
+        var forecastCard = $('<div>');
+
+        var headerForecast = $('<h3>');
+        headerForecast.text(dayjs.unix(data.list[index].dt).format("M/D/YYYY"));
+        
+        var tempForecast = $('<p>');
+        tempForecast.text("Temp: " + data.list[index].main.temp + "°F");
+        
+        var windForecast = $('<p>');
+        windForecast.text("Wind: " + data.list[index].wind.speed + " MPH");
+    
+        var humidForecast = $('<p>');
+        humidForecast.text("Humidity: " + data.list[index].main.humidity + "%");
+
+        forecastCard.append(headerForecast);
+        forecastCard.append(tempForecast);
+        forecastCard.append(windForecast);
+        forecastCard.append(humidForecast);
+
+        forecast.append(forecastCard);
+    }
+}
 
 submitBtn.on("click", formSubmission);
